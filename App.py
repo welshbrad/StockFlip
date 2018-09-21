b413615f5b9e00de7d6c620d3848d7949ea45f6a
Brad <welshbrad7@gmail.com>
2018-09-20 20:18:29 -0400

    Created Window and menus

--- App.py
+++ App.py
@@ -1,1 +1,33 @@
-import tkinter
+from tkinter import *
+
+
+# Create a window
+root = Tk() 
+
+
+def callback():
+    print("Function Callback()")
+
+def exit():
+    print("Exiting")
+    raise SystemExit
+
+
+# Create Menus
+menu = Menu(root)
+root.config(menu=menu)
+filemenu = Menu(menu)
+menu.add_cascade(label="File", menu=filemenu)
+filemenu.add_command(label="New", command=callback)
+filemenu.add_command(label="Open...", command=callback)
+filemenu.add_separator()
+filemenu.add_command(label="Exit", command=exit)
+
+helpmenu = Menu(menu)
+menu.add_cascade(label="Help", menu=helpmenu)
+helpmenu.add_command(label="About...", command=callback)
+
+
+
+
+root.mainloop()
