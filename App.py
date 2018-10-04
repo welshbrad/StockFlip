4410e4497b06a16372043ac2f94230c328952e05
Brad <welshbrad7@gmail.com>
2018-10-04 14:35:31 -0400

    Added Login GUI

--- App.py
+++ App.py
@@ -1,33 +1,59 @@
+from PIL import Image, ImageTk
 from tkinter import *
+from tkinter.ttk import Frame, Label, Style
+
+version = "0.1"
 
 
-# Create a window
-root = Tk() 
+class MainWindow(Frame):
+    bg_color = "#333"
+    fg_color = "#FFF"
+    padding = (10,10)
+  
+    def __init__(self):
+        super().__init__()   
+        self.master.title("StockFlip - version " + version)
+        self.initLogin()
+        #self.initUI()
+
+    def initLogin(self):
+          
+        self.master.configure(background=self.bg_color)
+        title_label = Label(self.master, text="Log in to StockFlip", background=self.bg_color, foreground=self.fg_color, justify=CENTER).grid(row=0, columnspan=2, padx=self.padding, pady=self.padding)
+        l1 = Label(self.master, text="Username", background=self.bg_color, foreground=self.fg_color).grid(row=1, column=0, padx=self.padding, pady=self.padding)
+        l2 = Label(self.master, text="Password", background=self.bg_color, foreground=self.fg_color).grid(row=2, column=0, padx=self.padding, pady=self.padding)
+        
+        
+        e1 = Entry(self.master).grid(row=1, column=1, padx=self.padding, pady=self.padding)
+        e2 = Entry(self.master).grid(row=2, column=1, padx=self.padding, pady=self.padding)
+        #Use this form to implement login
+
+        login_button = Button(self.master, text="Log in", command=lambda: Login()).grid(row=3, column=1, padx=self.padding, pady = self.padding, sticky=E)
+        
+        
+    def initUI(self):
+        
+      
+      
+        
+        self.pack(fill=BOTH, expand=1)
+      
+        Style().configure("TFrame", background="#333")
+   
+
+        label1 = Label(self, text="Test text").grid(column=1, row=1, sticky=W)
+        label2 = Label(self, text="Test text2").grid(column=2, row=1, sticky=E)
+        
+def Login():
+    print("Hi")
+        
+        
+def main():
+    
+    root = Tk()
+    app = MainWindow()
+    root.mainloop()  
 
 
-def callback():
-    print("Function Callback()")
-
-def exit():
-    print("Exiting")
-    raise SystemExit
-
-
-# Create Menus
-menu = Menu(root)
-root.config(menu=menu)
-filemenu = Menu(menu)
-menu.add_cascade(label="File", menu=filemenu)
-filemenu.add_command(label="New", command=callback)
-filemenu.add_command(label="Open...", command=callback)
-filemenu.add_separator()
-filemenu.add_command(label="Exit", command=exit)
-
-helpmenu = Menu(menu)
-menu.add_cascade(label="Help", menu=helpmenu)
-helpmenu.add_command(label="About...", command=callback)
-
-
-
-
-root.mainloop()
+if __name__ == '__main__':
+    main()  
