89c06a16fff3bdc92a17846681cad3c1110fa609
vunguyen04 <43426396+vunguyen04@users.noreply.github.com>
2018-10-20 10:31:56 -0400

    Update Login.py
    _Add isValidUsername function to check for a valid username (at least 4 character
    _Add isValidEmail function to check for a valid email format (example: abc@abc.abc)
    _Add isValidPassword to check for a valid password ( 4-32 characters, at least 1 upper and 1 lower-case letter, at least 1 number)

--- Login.py
+++ Login.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import hashlib
+import re
 import os
 userData = 'UserInfo.txt'
 
@@ -44,13 +45,39 @@ def CheckLogin(idEL, passEL):
 def CheckUsername(idE):
     with open(userData) as f:
         data = f.readlines()
-
         for x in range(1, len(data)-1, 4): #read from second line of the txt file and 4 line is a set of user
             id = data[x].rstrip()
             if idE == id:
-                return x; #if there is existed ID
-        return -1; #if there is no-existed ID
-   
+                return x #if there is existed ID
+        return -1 #if there is no-existed ID
+
+def isValidUsername (idE):
+    if (len(idE) < 4):
+        return False
+    else:
+        return True;
+def isValidPassword(passE):
+    while True:
+        if (len(passE)<8 or len(passE)>32):
+            return False
+        elif not re.search("[a-z]", passE):
+            return False
+        elif not re.search("[A-Z]", passE):
+            return False
+        elif not re.search("[0-9]", passE):
+            return False
+        elif re.search("\s", passE):
+            return False
+        else:
+            return True
+        
+    
+def isValidEmail(emailE):
+    if re.match('\S+@\S+.\S+', emailE):
+        return True
+    else:
+        return False
+
 def SignUp():
     global passE1
     global passE2
@@ -73,8 +100,8 @@ def SignUp():
     emailL.grid(row=4,column=0, sticky=W)
 
     idE = Entry(roots)
-    passE1 = Entry(roots)
-    passE2 = Entry(roots)
+    passE1 = Entry(roots, show='*')
+    passE2 = Entry(roots, show='*')
     emailE = Entry(roots)
     idE.grid(row = 1, column = 1)
     passE1.grid(row = 2, column = 1)
@@ -84,34 +111,73 @@ def SignUp():
 
     signupButton = Button(roots, text='Signup', command = FSSignup)
     signupButton.grid(columnspan = 2, sticky = W)
+    passE1.delete(0, END)
+    passE2.delete(0, END)
     roots.mainloop()
 
+
 def FSSignup():
-    #check Username open(userData) as f
-    check = CheckUsername(idE.get())
-    if check != -1:
+    #check Email
+    checkEmail = isValidEmail(emailE.get())
+    if checkEmail == False:
         roots = Tk()
-        roots.title('Error')
+        roots.title('Email Error')
+        error = Label(roots, text='Invalid Email Format (abc@abc.abc)')
+        error.grid(row=0, column=0, sticky=E)
+        return
+    #check Username
+    isValidUsernameFormat = isValidUsername(idE.get())
+    if isValidUsernameFormat == False:
+        roots = Tk()
+        roots.title('Username Error')
+        error = Label(roots, text='Invalid Username Format:')
+        ins1 = Label (roots, text='Username must contain at least 4 character')
+        error.grid(row=0, column=0, sticky=E)
+        ins1.grid(row=0, column=0, sticky=E)
+        return
+    checkUser = CheckUsername(idE.get())
+    if checkUser != -1:
+        roots = Tk()
+        roots.title('Username Error')
         error = Label(roots, text='Username already existed')
         error.grid(row=0, column=0, sticky=E)
     else:
+        #check Password match
         if (passE1.get() != passE2.get()):
             roots = Tk()
             roots.title('Error')
             error = Label(roots, text='Password does not match')
-            error.grid(row=0, column=0, sticky=E)
+            error.grid(row=0, column=0, sticky=W)
         else:
-            with open(userData, 'a') as f:
-                f.write(idE.get())
-                f.write('\n')
-                pass2write = hashing(passE1.get())
-                f.write(pass2write)
-                f.write('\n')
-                f.write(emailE.get())
-                f.write('\n')
-                f.write('\n') #extra line between two users' data
-                f.close()
+            #check valid password
+            checkPass = isValidPassword(passE1.get())
+            if checkPass == False:
                 roots = Tk()
-                roots.title('Success')
-                success = Label(roots, text='Account is created sucessfully')
+                roots.title('Password does not match')
+                
+                instruction = Label(roots, text='Password must contains:')
+                instruction.grid(row=0, column=0, sticky=W)
+                ins1 = Label(roots, text='_At least 1 lowercase character')
+                ins2 = Label(roots, text='_At least 1 uppercase character')
+                ins3 = Label(roots, text='_Length from 4 to 32 character')
+                
+                ins1.grid(row=1, column=0, sticky=W)
+                ins2.grid(row=2, column=0, sticky=W)
+                ins3.grid(row=3, column=0, sticky=W)
+                
+            else:
+                with open(userData, 'a') as f:
+                    f.write(idE.get())
+                    f.write('\n')
+                    pass2write = hashing(passE1.get())
+                    f.write(pass2write)
+                    f.write('\n')
+                    f.write(emailE.get())
+                    f.write('\n')
+                    f.write('\n') #extra line between two users' data
+                    f.close()
+                    roots = Tk()
+                    roots.title('Success')
+                    success = Label(roots, text='Account is created sucessfully')
+                    
                 success.grid(row=0,column=0,sticky=E)
