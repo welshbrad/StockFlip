11869d89a1b6c858f0f006bef7158a71dede587d
vunguyen04 <43426396+vunguyen04@users.noreply.github.com>
2018-11-10 02:39:17 -0500

    Add sendMessage
    _sendMessage can be used by administrator

--- EmailJob.py
+++ EmailJob.py
@@ -8,14 +8,13 @@ from email.mime.base import MIMEBase
 from email import encoders
 import re
 import os
-userData = 'UserInfo.txt'
 #Stock Flip info
 global from_address
-from_address='stockFlip@gmail.com' #stock flip email @ domain @gmail.com
+from_address='stockflip4020@gmail.com' #stock flip email @ domain @gmail.com
 global from_password
-from_password='emailPassword'
+from_password='Stockflip@4020'
 global from_name
-from_email='Stock Flip'
+from_name='Stock Flip'
 
 #send code for requesting password reset to the user email
 def sendCodeEmail(code, emailE, username):
@@ -63,3 +62,26 @@ def sendNotification(nameOfStock, emailE, username, state, value):
     server.sendmail(from_address, emailE, text)
 
     server.close()
+
+def sendMessage(username, emailE, subject, message):
+    to_address = emailE
+    to_name = username
+    msg = 'Dear ' +to_name+'\n'+message
+    
+    message = MIMEMultipart()
+    message['From'] = from_name
+    message['To'] = to_address
+    message['Subject'] = subject
+    message.attach(MIMEText(msg, 'plain'))
+
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+
+    #log in to the server
+    server.login(from_address, from_password)
+    text = message.as_string()
+    server.sendmail(from_address, emailE, text)
+
+    server.close()
+
+#sendMessage('vu3','vu3@mail.usf.edu','Test','TestMessage')
