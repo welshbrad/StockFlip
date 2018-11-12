690b8251d42f19be18b788bd46aebade6b66b021
vunguyen04 <43426396+vunguyen04@users.noreply.github.com>
2018-11-12 15:34:01 -0500

    Update database

--- database1.py
+++ database1.py
@@ -14,11 +14,22 @@ def check_user_password(inputUser, inputPassword):
     results = c.fetchall()
     return results
 
+
+def check_user_email(inputUser, inputEmail):
+    find_user_email = ('SELECT * FROM UserInfo WHERE username = ? AND email =?')
+    c.execute(find_user_email,[(inputUser), (inputEmail)])
+    results = c.fetchall()
+    return results
+
 def check_user(inputUser):
     find_user = ('SELECT * FROM UserInfo WHERE username = ?')
     c.execute(find_user,[(inputUser)])
     results = c.fetchall()
     return results
+
+def update_password(inputName, inputPassword):
+    c.execute('UPDATE UserInfo SET password = (?) WHERE username = (?)', (inputPassword, inputName))
+    db.commit()
 
 def check_email(inputEmail):
     find_email = ('SELECT * FROM UserInfo WHERE email = ?')
@@ -47,6 +58,14 @@ def find_credits(inputUser):
         print("")
     return i[1]
 
+def find_total_value(inputUser):
+    findTotalValue = ('SELECT * FROM UserPortfolio WHERE username = ?')
+    c.execute(findTotalValue,[(inputUser)])
+    results = c.fetchall()
+    for i in results:
+        print("")
+    return i[2]
+
 def update_credit(inputName, inputCredit):
     c.execute('UPDATE UserPortfolio SET num_credits = (?) WHERE username = (?)', (inputCredit, inputName))
     db.commit()
@@ -66,6 +85,10 @@ def find_stock_of_user(inputUser):
     results = c.fetchall()
     return results
 
+def update_stock(inputUser, stockName, amount):
+    c.execute('UPDATE UserStockAmount SET amount = (?) WHERE username = (?) AND stockname = (?)', (amount, inputUser, stockName))
+    db.commit()
+    
 c.execute('CREATE TABLE IF NOT EXISTS UserQuickAccess (username string, stockname string)')
 def insert_user_quick_access(name, stockName):
     c.execute('INSERT INTO UserQuickAccess (username, stockname) VALUES(?, ?)', (name, stockName))
@@ -77,5 +100,12 @@ def find_user_quick_access(inputUser):
     results = c.fetchall()
     return results
 
+def remove_user_quick_access(name, stockName):
+    c.execute('DELETE FROM UserQuickAccess where username = (?) AND stockname = (?)', (name, stockName))
+    db.commit()
+
+#insert_user_quick_access("brad", "AAPL")
+#c.execute('DELETE FROM UserStockAmount where stockname = "GOOGL"')
+#db.commit()
 #c.close
 #db.close()
